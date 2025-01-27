0953ae7bd0383200ae9dd1d444e7b8fc67a56ba6
João Felipe Maia Barbosa <103753367+joaofelipemb@users.noreply.github.com>
2025-01-27 17:47:48 +0000

    atualiza a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function Home() {
-    return <h1>Mamãe, antes de sair a Raiz disse que te ama! 
-        Inclusive, ela quer que você cuide dela enquanto eu estiver fora!</h1>
+    return <h1>Mamãe, antes de eu ir embora, a Raiz me disse que te ama! 
+        Inclusive, ela quer que você cuide dela enquanto eu estiver fora!
+        Ela adorou ser escovada por você hoje! :)</h1>
 }
 
 export default Home;
