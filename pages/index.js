8ced56bbc3637652072d4a886b04404255bf23ae
João Felipe Maia Barbosa <103753367+joaofelipemb@users.noreply.github.com>
2025-01-27 18:19:44 +0000

    teste de amend

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return <h1>Mamãe, antes de eu ir embora, a Raiz me disse que te ama! 
         Inclusive, ela quer que você cuide dela enquanto eu estiver fora!
-        Ela adorou ser escovada por você hoje! :)</h1>
+        Ela adorou ser escovada por você hoje! :))</h1>
 }
 
 export default Home;
