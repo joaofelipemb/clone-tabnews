38a2c0927906ab363b6f59d894091536cda02a9c
João Felipe Maia Barbosa <103753367+joaofelipemb@users.noreply.github.com>
2025-01-28 17:41:48 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 function Home() {
-    return <h1>Mamãe, antes de eu ir embora, a Raiz me disse que te ama! 
-        Inclusive, ela quer que você cuide dela enquanto eu estiver fora!
-        Ela adorou ser escovada por você hoje! :))</h1>
+  return (
+    <h1>
+      Mamãe, antes de eu ir embora, a Raiz me disse que te ama! Inclusive, ela
+      quer que você cuide dela enquanto eu estiver fora! Ela adorou ser escovada
+      por você hoje! :))
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identacaoerrada");
 }
 
 export default Home;
